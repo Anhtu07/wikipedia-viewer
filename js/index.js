454c4d1e3737bec6c836bbed3361ceee46111069
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-05-23 22:54:27 +0700

    log to console
    log page_title, page_description and page_link to console

--- js/index.js
+++ js/index.js
@@ -3,4 +3,8 @@
       var string = document.getElementById("search-box").value;
       console.log(string);
     });
+      var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=zyz&limit=1&namespace=0&format=jsonfm?callback=?"
+      $.getJSON(link, function(json) {
+        console.log(JSON.stringify(json));
+      });
   });
