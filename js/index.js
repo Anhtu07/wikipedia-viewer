56e893ec0b6a2541d903a10155b953848a030972
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-05-24 21:19:22 +0700

    function page
    look a lot more like a website for wiki search

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
-  $(document).ready(function() {
-    $("#button").on("click", function(){
-      var string = document.getElementById("search-box").value;
-      console.log(string);
-    });
-      var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=zyz&limit=1&namespace=0&format=jsonfm?callback=?"
-      $.getJSON(link, function(json) {
-        console.log(JSON.stringify(json));
-      });
+$(document).ready(function() {
+  var string = "";
+  $("#button").on("click", function() {
+    string = document.getElementById("search-box").value;
+    $.getJSON(
+      "https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&search=" +
+        string +
+        "&callback=?",
+      function(json) {
+        var content = "";
+        var page_titles = json[1];
+        var page_descriptions = json[2];
+        var page_link = json[3];
+        for(var i = 0; i < page_titles.length; i++)
+          {
+            content += "<div style = 'font-size: 10px; background-color: #D3D3D3; text-align:left; margin-left: 7%; margin-right: 7%; margin-top: 30px; border-style: solid; border-right: 5px; border-top: 5px; border-bottom: 5px; border-radius: 2px'> <a href = " + page_link[i] + ">" + "<h1 style='font-size: 75%'>" + page_titles[i] + "</h1>" + "</a>" +"<p>" + page_descriptions[i] + "</p>" + "</div>";
+          }
+        $("#button").html(content);
+        console.log(JSON.stringify(json[3]));
+      }
+    );
   });
+});
